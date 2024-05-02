0dc3810cf516a0b1512495465a165474d8fd3ce8
Maarten de Ruyter <devops@geodatagarden.com>
2024-05-02 10:30:27 +0200

    1 more black issue fixed

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -1,9 +1,8 @@
 """init file errors module."""
-__version__ = "1.3.0"
+
+__version__ = "1.3.3"
 
 from errors.base import add_error_data, is_error  # noqa F401
 from errors.data_classes import ReturnValueWithErrorStatus  # noqa F401
 from errors.data_classes import ReturnValueWithStatus  # noqa F401
 from errors.error import ErrorCode, ListErrors  # noqa F401
-
-__version__ = "1.3.3"
