a3408122f977e91a6220e015d0241f728d2c4659
Maarten de Ruyter <devops@geodatagarden.com>
2024-05-23 11:22:51 +0200

    added ErrorListByMixin to toplevel module import

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -6,3 +6,4 @@ from errors.base import add_error_data, is_error  # noqa F401
 from errors.data_classes import ReturnValueWithErrorStatus  # noqa F401
 from errors.data_classes import ReturnValueWithStatus  # noqa F401
 from errors.error import ErrorCode, ListErrors  # noqa F401
+from errors.mixin import ErrorListByMixin
