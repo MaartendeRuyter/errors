4241852fd1c72489835407fd19d1d0705005364b
Maarten de Ruyter <devops@geodatagarden.com>
2024-05-23 11:25:29 +0200

    Improved package default imports
    Improved linting with __all__ attributed
    added FunctionalErrorsBaseClass

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -2,8 +2,18 @@
 
 __version__ = "1.3.5"
 
-from errors.base import add_error_data, is_error  # noqa F401
-from errors.data_classes import ReturnValueWithErrorStatus  # noqa F401
-from errors.data_classes import ReturnValueWithStatus  # noqa F401
-from errors.error import ErrorCode, ListErrors  # noqa F401
+from errors.base import FunctionalErrorsBaseClass, add_error_data, is_error
+from errors.data_classes import ReturnValueWithErrorStatus, ReturnValueWithStatus
+from errors.error import ErrorCode, ListErrors
 from errors.mixin import ErrorListByMixin
+
+__all__ = [
+    "add_error_data",
+    "is_error",
+    "ReturnValueWithErrorStatus",
+    "ReturnValueWithStatus",
+    "ErrorCode",
+    "ListErrors",
+    "ErrorListByMixin",
+    "FunctionalErrorsBaseClass",
+]
